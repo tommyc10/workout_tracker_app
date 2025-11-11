bf8698ab7d21729277fc9986c915dbc7688db790
Tommy Clark <tommyclark362@gmail.com>
2025-11-11 20:31:10 +0000

    ui improvements

--- src/components/shared/BtnLink.tsx
+++ src/components/shared/BtnLink.tsx
@@ -8,7 +8,7 @@ export const BtnLink = ({ href, text, className = "" }: BtnLinkProps) => {
   return (
     <a
       href={href}
-      className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border dark:bg-violet-600 cursor-pointer ${className}`}
+      className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border dark:bg-violet-600 cursor-pointer  ${className}`}
     >
       <span className="relative z-10 text-white"> {text}</span>
     </a>
